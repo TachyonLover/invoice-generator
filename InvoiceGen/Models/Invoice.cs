57cbff97db2a4798963f17e70cbe0a0145b10f6b
TachyonLover <robertflores571@gmail.com>
2026-08-13 01:42:03 -0400

    Add project files.

---
+++ InvoiceGen/Models/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceGen.Models
+{
+    public class Invoice
+    {
+        public string InvoiceNumber { get; set; } = "";
+        public DateTime Date { get; set; }
+
+        public string CustomerName { get; set; } = "";
+        public string CustomerAddress { get; set; } = "";
+
+        public List<InvoiceItem> Items { get; set; } = new();
+
+        public decimal Total
+        {
+            get
+            {
+                return Items.Sum(item => item.Total);
+            }
+        }
+    }
+}
---
+++ InvoiceGen/Models/InvoiceItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceGen.Models
+{
+    public class InvoiceItem
+    {
+        public string Description { get; set; } = "";
+        public int Quantity { get; set; }
+        public decimal PricePerUnit { get; set; }
+
+        public decimal Total
+        {
+            get
+            {
+                return Quantity * PricePerUnit;
+            }
+        }
+    }
+}
